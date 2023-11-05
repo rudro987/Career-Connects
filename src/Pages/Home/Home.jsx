e6a82179d642970e920963a1b2da860981607fa9
Tanvir Rudro <rudro987@gmail.com>
2023-11-05 07:43:42 +0600

    Applications initial skeleton setup completed

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-5xl font-bold">This is Home Route</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Routes/routes.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../Layout/MainLayout';
+import ErrorPage from '../Pages/ErrorPage/ErrorPage';
+import Home from '../Pages/Home/Home';
+
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            },
+            
+        ]
+        
+    }
+]);
+
+export default routes;
